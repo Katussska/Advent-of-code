6583e068f9f6dfdf7b24a63782faf241d85cdd9c
katussska <katkabaier@gmail.com>
2024-12-03 13:08:19 +0100

    2024/03/1 setup
    Took 11 minutes

---
+++ 2024/day_3/src/index.ts
@@ -0,0 +1,17 @@
+async function partOne() {
+
+}
+
+
+async function partTwo() {
+
+}
+
+async function main() {
+    console.log("PART ONE");
+    await partOne();
+    console.log("PART TWO");
+    await partTwo();
+}
+
+main();

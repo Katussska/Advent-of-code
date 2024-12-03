7e2fa483af9922a46e2172da4c2439757e3430a0
katussska <katkabaier@gmail.com>
2024-12-03 12:53:10 +0100

    2024/02/2 done
    Took 13 minutes

--- 2024/day_2/src/index.ts
+++ 2024/day_2/src/index.ts
@@ -5,7 +5,7 @@ async function isSafeReport(numbers: number[]): Promise<boolean> {
     let isIncreasing = true;
     let isDecreasing = true;
 
-    for (let i = 1; i <= numbers.length; i++) {
+    for (let i = 1; i < numbers.length; i++) {
         const diff = Math.abs(numbers[i] - numbers[i - 1]);
 
         if (diff < 1 || diff > 3)
@@ -30,19 +30,48 @@ async function partOne() {
     });
 
     let sum = 0;
+    const unsafeReports: number[][] = [];
 
     for await (const line of rl) {
         const numbers = line.split(' ').map(Number);
-        if (await isSafeReport(numbers))
+        if (await isSafeReport(numbers)) {
             sum++;
+        } else {
+            unsafeReports.push(numbers);
+        }
     }
 
     console.log("Number of safe reports: ", sum);
+    return unsafeReports;
+}
+
+async function isSafeWithDampener(numbers: number[]): Promise<boolean> {
+    for (let i = 0; i < numbers.length; i++) {
+        const modifiedNumbers = numbers.slice(0, i).concat(numbers.slice(i + 1));
+        if (await isSafeReport(modifiedNumbers)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+async function partTwo(unsafeReports: number[][]) {
+    let sum = 0;
+
+    for (const report of unsafeReports) {
+        if (await isSafeWithDampener(report)) {
+            sum++;
+        }
+    }
+
+    console.log("Number of safe reports with Problem Dampener: ", sum);
 }
 
 async function main() {
     console.log("PART ONE");
-    await partOne();
+    const unsafeReports = await partOne();
+    console.log("PART TWO");
+    await partTwo(unsafeReports);
 }
 
 main();
